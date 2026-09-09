d7d4814baa640c481dd7110f0ef85b1df9eba094
VoidrainStuidos <furbyailesi@gmail.com>
2026-09-09 19:48:21 +0300

    Add files via upload

---
+++ PointFunction.py
@@ -0,0 +1,11 @@
+class point:
+
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+ob1 = point(5, 9)
+print(ob1)
